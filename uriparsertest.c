0bb39e210e247834a5e7a24f190e4d551f60ac48
nctl144 <nctl144@gmail.com>
2018-05-25 11:17:59 -0400

    fix the compiling errors

--- uriparsertest.c
+++ uriparsertest.c
@@ -1,19 +1,24 @@
+#include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "uriparser/include/uriparser/Uri.h"
+#include <uriparser/Uri.h>
 // #include <UriBase.h>
 // #include <UriDefsAnsi.h>
 // #include <UriDefsConfig.h>
 // #include <UriDefsUnicode.h>
 // #include <UriIp4.h>
 
-UriParserStateA state;
-UriUriA uri;
+int main() {
+    UriParserStateA state;
+    UriUriA uri;
 
-state.uri = &uri;
+    state.uri = &uri;
 
-if (uriParseUriA(&state, "file:///home/user/song.mp3") != URI_SUCCESS) {
+    if (uriParseUriA(&state, "file:///home/user/song.mp3") != URI_SUCCESS) {
+        uriFreeUriMembersA(&uri);
+    }
+
     uriFreeUriMembersA(&uri);
+    printf("%s", uri);
+    return 0;
 }
-
-uriFreeUriMembersA(&uri);
